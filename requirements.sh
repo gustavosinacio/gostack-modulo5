90244da0b63f4a786defc022e55f91d629703a66
gustavo <gsinacio94@gmail.com>
2019-07-14 20:03:28 -0300

    Finishing video 9 - listing repos

---
+++ requirements.sh
@@ -0,0 +1,5 @@
+yarn add \
+react-router-dom \
+styled-components \
+react-icons \
+axios
---
+++ src/pages/Main/index.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { FaGithubAlt, FaPlus, FaSpinner } from 'react-icons/fa';
+
+import api from '../../services/api';
+
+import { Container, Form, SubmitButton, List } from './styles';
+
+export default class Main extends React.Component {
+  state = {
+    newRepo: '',
+    repositories: [],
+    loading: false,
+  };
+
+  handleInputChange = e => {
+    this.setState({ newRepo: e.target.value });
+  };
+
+  handleSubmit = async e => {
+    e.preventDefault();
+
+    this.setState({ loading: true });
+
+    const { newRepo, repositories } = this.state;
+
+    const response = await api.get(`/repos/${newRepo}`);
+
+    const { full_name } = response.data;
+    const data = { name: full_name };
+
+    this.setState({
+      repositories: [...repositories, data],
+      newRepo: '',
+      loading: false,
+    });
+  };
+
+  render() {
+    const { newRepo, repositories, loading } = this.state;
+    return (
+      <Container>
+        <h1>
+          <FaGithubAlt />
+          Repositories
+        </h1>
+
+        <Form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            placeholder="Adicionar Repositório"
+            value={newRepo}
+            onChange={this.handleInputChange}
+          />
+          <SubmitButton loading={loading}>
+            {loading ? (
+              <FaSpinner color="#FFF" size={14} />
+            ) : (
+              <FaPlus color="#FFF" size={14} />
+            )}
+          </SubmitButton>
+        </Form>
+
+        <List>
+          {repositories.map(repo => (
+            <li key={`${repo.name}`}>
+              <span>{repo.name}</span>
+              <a href="/">Detalhes</a>
+            </li>
+          ))}
+        </List>
+      </Container>
+    );
+  }
+}
